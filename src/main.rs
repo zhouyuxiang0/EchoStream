707b72ebfdfe0a82d5da822c28e3d3651c3ad5a1
zhouyuxiang <zhouyuxiang@epaynfc.com>
2024-04-06 22:29:05 +0800

    test

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 #![allow(non_snake_case)]
 
-use dioxus::prelude::*;
+use dioxus::{
+    desktop::{tao::platform::windows::WindowBuilderExtWindows, Config, WindowBuilder},
+    prelude::*,
+};
 use log::LevelFilter;
 
 #[derive(Clone, Routable, Debug, PartialEq)]
@@ -14,8 +17,15 @@ enum Route {
 fn main() {
     // Init debug
     dioxus_logger::init(LevelFilter::Info).expect("failed to init logger");
-
-    dioxus::launch(App);
+    LaunchBuilder::desktop()
+        .with_cfg(
+            Config::new().with_window(
+                WindowBuilder::new()
+                    .with_decorations(true)
+                    .with_title("EchoStream"),
+            ),
+        )
+        .launch(App);
 }
 
 #[component]
@@ -33,14 +43,23 @@ fn Blog(id: i32) -> Element {
 
 #[component]
 fn Home() -> Element {
-    let mut count = use_signal(|| 0);
+    // let mut count = use_signal(|| 0);
 
     rsx! {
-        Link { to: Route::Blog { id: count() }, "Go to blog" }
-        div {
-            h1 { "High-Five counter: {count}" }
-            button { onclick: move |_| count += 1, "Up high!" }
-            button { onclick: move |_| count -= 1, "Down low!" }
+        div { class: "menu-button",
+            svg {
+                "xmlns": "http://www.w3.org/2000/svg",
+                "stroke-linejoin": "round",
+                "fill": "none",
+                "stroke-width": "2",
+                "stroke-linecap": "round",
+                "stroke": "currentColor",
+                "viewBox": "0 0 24 24",
+                path { "d": "M13 2H6a2 2 0 0 0-2 2v16c0 1.1.9 2 2 2h12a2 2 0 0 0 2-2V9l-7-7z" }
+                path { "d": "M13 3v6h6" }
+            }
+            div { class: "plus", "+" }
+            span { "连接" }
         }
     }
 }
